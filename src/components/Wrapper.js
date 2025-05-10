604a688238551e390e551b64bfea272123cf578c
Anton Buketov <38258424+KotBegemotEST@users.noreply.github.com>
2025-05-10 11:48:03 +0300

    Delete src directory

--- src/components/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../styles/global.css'
-import '../styles/statistic.css'
-
-const Wrapper = ({ children }) => {
-  return <div className="main-wrapper">{children}</div>;
-};
-
-export default Wrapper;
